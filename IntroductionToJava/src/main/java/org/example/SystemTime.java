28fd78e12e67c0df3d3b1d39c5743bf3c39b5e92
longg2504 <longg2504@gmail.com>
2023-04-07 11:14:09 +0700

    SystemTime

---
+++ IntroductionToJava/src/main/java/org/example/SystemTime.java
@@ -0,0 +1,12 @@
+package org.example;
+import javax.xml.crypto.Data;
+import java.util.Scanner;
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.println("now is : " + now );
+    }
+}
