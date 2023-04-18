0edc653f326898cc3c9a929dc2d51c789f7f0ed3
longg2504 <longg2504@gmail.com>
2023-04-18 13:54:31 +0700

    Point2DAnd3D

---
+++ Inheritance/src/main/java/Point2DAnd3D/TestClass.java
@@ -0,0 +1,11 @@
+package Point2DAnd3D;
+
+public class TestClass {
+    public static void main(String[] args) {
+        Point2D first2DPoint=new Point2D(3, 4);
+        Point3D first3DPoint=new Point3D(4, 5,7);
+
+        System.out.println(first2DPoint.toString());
+        System.out.println(first3DPoint.toString());
+    }
+}
