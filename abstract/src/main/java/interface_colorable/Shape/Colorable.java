421b99a26c513289f6825bdc731e96178553dcd0
longg2504 <longg2504@gmail.com>
2023-04-21 15:22:41 +0700

    InterfaceColorable

---
+++ abstract/src/main/java/interface_colorable/Shape/Colorable.java
@@ -0,0 +1,5 @@
+package interface_colorable.Shape;
+
+public interface Colorable {
+    public abstract void howToColor();
+}
---
+++ abstract/src/main/java/interface_colorable/Test.java
@@ -0,0 +1,30 @@
+package interface_colorable;
+import  interface_colorable.Shape.*;
+
+public class Test {
+    public static void main(String[] args) {
+        Shape[] shape = new Shape[10];
+        for (int i = 0; i < 10; i++) {
+            shape[i] = getRandomShape();
+        }
+        Shape.printShape(shape);
+        for (Shape x : shape) {
+            if (x instanceof Colorable) {
+                ((Colorable) x).howToColor();
+            }
+        }
+
+    }
+
+    static Shape getRandomShape() {
+        int radom = (int) (Math.round(Math.random() * 2));
+        switch (radom) {
+            case 0:
+                return new Square(10);
+            case 1:
+                return new Rectangle(10, 20);
+            default:
+                return new Circle(10);
+        }
+    }
+}
