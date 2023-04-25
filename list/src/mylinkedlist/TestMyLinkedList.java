6e745427d0b49c9a23ec76b773e9283eba59a91f
longg2504 <longg2504@gmail.com>
2023-04-25 11:45:22 +0700

    MyLinkedList

---
+++ list/src/mylinkedlist/TestMyLinkedList.java
@@ -0,0 +1,20 @@
+package mylinkedlist;
+
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedList ll = new MyLinkedList(10);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+        ll.addFirst(50);
+        ll.addFirst(61);
+
+
+
+        ll.add(4,9);
+        ll.add(4,9);
+        ll.add(5,20);
+        ll.printList();
+    }
+}
